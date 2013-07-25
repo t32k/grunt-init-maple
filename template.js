facc6083ed997779c1ce50b227c04ca70f4e5e7c
t32k <ijok.ijok@gmail.com>
2013-07-25 20:40:51 +0900

    Fix .ai writing error

--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 /*
  * grunt-init-maple
- * https://github.com/t32k/grunt-init-maple
+ * https://github.com/t32k/grunt-init-maple/
  *
  * Copyright (c) 2013 Koji Ishimoto
  * Licensed under the MIT license.
@@ -9,47 +9,25 @@
 'use strict';
 
 // Basic template description.
-exports.description = 'Create a basic Maple project.';
+exports.description = 'Create a basic maple project.';
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = 'This template tries to guess file and directory paths, but ' +
-  'you will most likely need to edit the generated maple.js file before ' +
-  'running grunt. _If you run grunt after generating the maple, and ' +
-  'it exits with errors, edit the file!_';
+exports.notes = '';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = '';
 
 // Any existing file or directory matching this wildcard will cause a warning.
-exports.warnOn = 'maple.js';
+exports.warnOn = '*';
 
 // The actual init template.
 exports.template = function(grunt, init, done) {
 
   init.process({}, [
     // Prompt for these values.
-    {
-      name: 'dom',
-      message: 'Is the DOM involved in ANY way?',
-      default: 'Y/n',
-      warning: 'Yes: QUnit unit tests + JSHint "browser" globals. No: Nodeunit unit tests.'
-    }
+    init.prompt('project_name'),
+    init.prompt('author_name')
   ], function(err, props) {
-    props.dom = /y/i.test(props.dom);
-    props.file_name = props.package_json ? '<%= pkg.name %>' : 'FILE_NAME';
-
-    // Find the first `preferred` item existing in `arr`.
-    function prefer(arr, preferred) {
-      for (var i = 0; i < preferred.length; i++) {
-        if (arr.indexOf(preferred[i]) !== -1) {
-          return preferred[i];
-        }
-      }
-      return preferred[0];
-    }
-
-    // Guess at some directories, if they exist.
-    var dirs = grunt.file.expand({filter: 'isDirectory'}, '*').map(function(d) { return d.slice(0, -1); });
-    props.lib_dir = prefer(dirs, ['lib', 'src']);
-    props.test_dir = prefer(dirs, ['test', 'tests', 'unit', 'spec']);
-
 
     // Files to copy (and process).
     var files = init.filesToCopy(props);
